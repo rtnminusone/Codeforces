3aa65d32f4b82933d764eae9911b9249e76c92ce
returnminusone <returnminusone@outlook.com>
2026-03-08 01:19:47 +0900

    initial commit

---
+++ 2172m/2172m.cs
@@ -0,0 +1,60 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618, CS8620
+
+class Program
+{
+	public static int N, M, K;
+	public static int[] T, R;
+	public static bool[] V;
+	public static List<int>[] L;
+	public static Queue<(int, int)> Q = new Queue<(int, int)>();
+
+	public static void BFS()
+	{
+		while (Q.Count > 0)
+		{
+			var (q, w) = Q.Dequeue();
+
+			R[T[q]] = w;
+
+			if (L[q] == null) continue;
+			foreach (int l in L[q])
+			{
+				if (V[l]) continue;
+				Q.Enqueue((l, w + 1));
+				V[l] = true;
+			}
+		}
+	}
+
+	public static void Main()
+	{
+		string[] S = Console.ReadLine().Split();
+		N = int.Parse(S[0]);
+		M = int.Parse(S[1]);
+		K = int.Parse(S[2]);
+		T = new int[N];
+		R = new int[K];
+		V = new bool[N];
+		L = new List<int>[N];
+		S = Console.ReadLine().Split();
+		for (int i = 0; i < N; i++)
+		{
+			T[i] = int.Parse(S[i]) - 1;
+		}
+		for (int i = 0; i < M; i++)
+		{
+			S = Console.ReadLine().Split();
+			int left = int.Parse(S[0]) - 1;
+			int right = int.Parse(S[1]) - 1;
+			(L[left] ??= new List<int>()).Add(right);
+			(L[right] ??= new List<int>()).Add(left);
+		}
+
+		Q.Enqueue((0, 0));
+		V[0] = true;
+
+		BFS();
+
+		Console.WriteLine(string.Join(" ", R));
+	}
+}
---
+++ 3a/3a.cs
@@ -0,0 +1,79 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618, CS8620
+
+class Program
+{
+	public class Pos
+	{
+		public int x;
+		public int y;
+		public string s;
+		public Pos? p;
+
+		public Pos(int x, int y, string s, Pos? p)
+		{
+			this.x = x;
+			this.y = y;
+			this.s = s;
+			this.p = p;
+		}
+	}
+
+	public static int N = 8, X, Y;
+	public static bool[,] V = new bool[N, N];
+	public static Queue<Pos> Q = new Queue<Pos>();
+
+	public static int[] dx = { -1, -1, -1, 0, 0, 1, 1, 1 };
+	public static int[] dy = { -1, 0, 1, -1, 1, -1, 0, 1 };
+	public static string[] dt = { "LU", "U", "RU", "L", "R", "LD", "D", "RD" };
+
+	public static Pos Create(int x, int y, string s, Pos p)
+	{
+		if (x < 0 || x >= N || y < 0 || y >= N) return null;
+		if (V[x, y]) return null;
+
+		return new Pos(x, y, s, p);
+	}
+
+	public static void BFS()
+	{
+		while (Q.Count > 0)
+		{
+			Pos p = Q.Dequeue();
+
+			if (p.x == X && p.y == Y)
+			{
+				List<string> L = new List<string>();
+				while (p != null)
+				{
+					L.Add(p.s);
+					p = p.p;
+				}
+				L.Reverse();
+				Console.WriteLine((L.Count - 1) + string.Join("\n", L));
+				return;
+			}
+
+			for (int i = 0; i < 8; i++)
+			{
+				Pos nextp = Create(p.x + dx[i], p.y + dy[i], dt[i], p);
+				if (nextp == null) continue;
+				Q.Enqueue(nextp);
+				V[nextp.x, nextp.y] = true;
+			}
+		}
+	}
+
+	public static void Main()
+	{
+		string S = Console.ReadLine();
+		Y = S[0] - 'a';
+		X = 7 - (S[1] - '1');
+		Q.Enqueue(new Pos(X, Y, "", null));
+		V[X, Y] = true;
+		S = Console.ReadLine();
+		Y = S[0] - 'a';
+		X = 7 - (S[1] - '1');
+
+		BFS();
+	}
+}
